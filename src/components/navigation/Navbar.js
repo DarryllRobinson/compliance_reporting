1e663d5337b17af167d71f46525097896ccf4565
DarryllRobinson <darryllrobinson@icloud.com>
2025-04-07 12:51:23 +1000

    Refactor Navbar component to include responsive menu and theme toggle functionality

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
   Typography,
   IconButton,
+  Box,
+  Button,
+  Menu,
+  MenuItem,
   useTheme,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { Link } from "react-router";
 
 export default function Navbar({ isDarkTheme, onToggleTheme }) {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position="static">
@@ -18,6 +33,35 @@ export default function Navbar({ isDarkTheme, onToggleTheme }) {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Placeholder Logo
         </Typography>
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/report">
+            Report Form
+          </Button>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose} component={Link} to="/">
+              Home
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose} component={Link} to="/report">
+              Report Form
+            </MenuItem>
+          </Menu>
+        </Box>
         <IconButton
           sx={{ ml: 1 }}
           onClick={onToggleTheme}
